105ab93ca56bc23072a3771a427ae2fb41a24c8f
LLEPT <103839621+lauuu-pt@users.noreply.github.com>
2023-03-26 22:55:49 +0100

    commit

--- SI-proj2-52.py
+++ SI-proj2-52.py
@@ -20,17 +20,58 @@ c = line(2,3,0,1,4)
 fronteira = quadro(0,0,10)
 obstaculos=fronteira | l | c   
 print(display(pacman=(1,1),pastilha=(1,6),obstaculos=fronteira | l | c ,path=[]))
-def planeia_online(pacman, pastilha, obstaculos):
-    self.pacman=pacman
-    self.pastilha=pastilha
-    self.obstaculos=obstaculos
-    pass
-def __init__(self, initial = ((1,1),0), goal=1, pastilhas={}, obstacles=quadro(0,0,10), dim=10):
-        self.initial = initial
-        self.pacman = initial[0] # (x,y) que representa a posição inicial do Pacman
-        self.pontos = initial[1]
-        self.goal = goal # o nº mínimo de pontos que o Pacman deve obter
-        self.pastilhas = pastilhas # conjunto das pastilhas: dicionário com chaves {N,D,C} e os valores são uma lista de tuplos correspondentes às posições das pastilhas
-        self.obstacles = obstacles # conjunto de obstáculos
-        self.dim = dim # dimensão do mundo
 
+""" def modelo(pacman,pastilha):
+        xpacman=pacman[0]
+        ypacman=pacman[1]
+        xpastilha=pastilha[0]
+        ypastilha=pastilha[1]
+        maiorx=max(xpacman,xpastilha)
+        maiory=max(ypacman,ypastilha)
+        menorx=min(xpacman,xpastilha)
+        menory=min(ypacman,ypastilha)
+        maiores=(maiorx,maiory)
+        menores=(menorx,menory)
+        return menores , maiores
+print(modelo(pacman=(1,1),pastilha=(1,6))) """
+
+
+def modelo(pacman,pastilha):
+        xpacman=pacman[0]
+        ypacman=pacman[1]
+        xpastilha=pastilha[0]
+        ypastilha=pastilha[1]
+        maiorx=max(xpacman,xpastilha)
+        maiory=max(ypacman,ypastilha)
+        menorx=min(xpacman,xpastilha)
+        menory=min(ypacman,ypastilha)
+        maiores=(maiorx,maiory)
+        nlinhas=maiorx
+        ncolunas=maiory
+        grid = [['.'] * ncolunas for i in range(nlinhas)]   
+        for nlinhas in grid:
+                print(' '.join([str(elem) for elem in nlinhas]))
+                
+                
+       # return  grid
+          
+        
+        
+        
+print(modelo(pacman=(1,1),pastilha=(1,6)))     
+        
+
+        
+        
+              
+def planeia_online(pacman, pastilha, obstaculos):
+    
+    pass
+
+
+
+def __init__(self, initial=(1, 1), goal=(7, 8), obstacles=()):
+        self.initial=initial
+        self.goal=goal 
+        self.obstacles=set(obstacles) - {initial, goal}
+        self.pacman = initial[0]
